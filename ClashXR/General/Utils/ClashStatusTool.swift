85cf070dbeea1f41f9a920f4bfb32e9ff5cb96dc
ParadiseDuo <>
2020-07-19 15:30:21 +0800

    fix tanslate error

--- ClashXR/General/Utils/ClashStatusTool.swift
+++ ClashXR/General/Utils/ClashStatusTool.swift
@@ -14,8 +14,8 @@ class ClashStatusTool {
         if cfg.usedHttpPort == 0 {
             Logger.log("checkPortConfig: \(cfg.mixedPort) ", level: .error)
             let alert = NSAlert()
-            alert.messageText = NSLocalizedString("ClashX Start Error!", comment: "")
-            alert.informativeText = NSLocalizedString("Ports Open Fail, Please try to restart ClashX", comment: "")
+            alert.messageText = NSLocalizedString("ClashXR Start Error!", comment: "")
+            alert.informativeText = NSLocalizedString("Ports Open Fail, Please try to restart ClashXR", comment: "")
             alert.addButton(withTitle: NSLocalizedString("Quit", comment: ""))
             alert.addButton(withTitle: "Edit Config")
             let ret = alert.runModal()
